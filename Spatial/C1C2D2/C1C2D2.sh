a73dad7d2cd014e2bdb8c00a647814d26af6b940
Tang Qian <39941019+qt37t247@users.noreply.github.com>
2023-05-09 13:27:24 +0800

    Add files via upload

---
+++ Spatial/C1C2D2/C1C2D2.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+#PBS -q parallel12
+#PBS -l select=1:ncpus=12:mem=45GB
+#PBS -l walltime=720:00:00
+#PBS -j oe
+
+cd $PBS_O_WORKDIR
+source /etc/profile.d/rec_modules.sh
+module load miniconda
+for i in {4801..4900}
+do
+source activate geog
+cd /hpctmp/dbstq/HLG/C1C2D2
+sed "s/%SEED%/$i/" C1C2D2_params.template.py > C1C2D2_params.py
+python C1C2D2.py
+cd ./GNX_mod-C1C2D2_params/it--1/spp-spp_0/
+cp mod-C1C2D2_params_it--1_spp-spp_0_OTHER_STATS.csv /hpctmp/dbstq/HLG/C1C2D2/run"$i"_count.csv
+source activate R
+for j in *.vcf
+do base=${j%.vcf*}
+vcftools --vcf "$base".vcf --site-pi --out ${base}
+cp "$base".sites.pi /hpctmp/dbstq/HLG/C1C2D2/run"$i"_"$base".sites.pi
+done
+done
+
+module load R-4.0.4
+cd /hpctmp/dbstq/HLG/C1C2D2/
+R CMD BATCH data_collect.R
---
+++ Spatial/C1C5D1/data_collect.R
@@ -0,0 +1,83 @@
+files <- list.files(pattern = "count.csv$", recursive = T)
+counts <- do.call("cbind", lapply(files, function(fn){data.frame(Filename=fn, read.csv(fn, header = T))}))
+write.csv(counts, "Raw_counts.csv")
+ConfI <- data.frame(matrix(ncol = 3, nrow = 110), row.names = counts[,"t"])
+colnames(ConfI) <- c("Mean", "Lower", "Upper") 
+
+######If use quantile###############################################
+for (i in 1:110){
+  ConfI[i,1] <- mean(as.numeric(counts[,c(1:100)*3][i,]))
+  ConfI[i,2] <- quantile(as.numeric(counts[,c(1:100)*3][i,]), 0.025)
+  ConfI[i,3] <- quantile(as.numeric(counts[,c(1:100)*3][i,]), 0.975)
+}
+
+######If use SD#####################################################
+#for (i in 1:110){
+#  ConfI[i,1] <- mean(as.numeric(counts[,c(1:100)*3][i,]))
+#  ConfI[i,2] <- ConfI[i,1]-sd(as.numeric(counts[,c(1:100)*3][i,]))
+#  ConfI[i,3] <- ConfI[i,1]+sd(as.numeric(counts[,c(1:100)*3][i,]))
+#}
+####################################################################
+
+ConfI[,"t"] <- counts[,"t"] 
+write.csv(ConfI, "q025_counts.csv")
+
+
+###Calculate nucleotide diversity
+filez <- list.files(pattern = "sites.pi$", recursive = T)
+pis <- do.call("cbind", lapply(filez, function(fn){data.frame(Filename=fn, read.table(fn, header = T))}))
+write.csv(pis, "Raw_pis.csv")
+Pis <- data.frame(matrix(ncol = 100, nrow = 23), row.names = sort(append(as.character(1:22*5-5), "109")))
+for (j in 1:100){
+  for (i in 1:23){
+    Pis[i,j] <- mean(2*pis[,i*4+(j)*92]*(1-pis[,i*4+(j)*92]))
+  }
+}
+ConfIP <- data.frame(matrix(ncol = 3, nrow = 23), row.names = sort(append(as.character(1:22*5-5), "109")))
+colnames(ConfIP) <- c("Mean", "Lower", "Upper") 
+
+for (i in 1:23){
+  ConfIP[i,1] <- mean(as.numeric(Pis[i,]))
+  ConfIP[i,2] <- quantile(as.numeric(Pis[i,]), 0.025)
+  ConfIP[i,3] <- quantile(as.numeric(Pis[i,]), 0.975)
+}
+######If use SD#####################################################
+#for (i in 1:23){
+#  ConfIP[i,1] <- mean(unlist((c(pis[,(1:100)*92+i*4]))))
+#  ConfIP[i,2] <- ConfIP[i,1]-sd(unlist((c(pis[,(1:100)*92+i*4]))))
+#  ConfIP[i,3] <- ConfIP[i,1]+sd(unlist((c(pis[,(1:100)*92+i*4]))))
+#}
+####################################################################
+
+ConfIP[,"t"] <- as.numeric(rownames(ConfIP)) 
+ConfIP <- ConfIP[order(ConfIP$t),]
+write.csv(ConfIP, "q025_pis.csv")
+
+###Calculate number of fixed loci
+Fixed <- data.frame(matrix(ncol = 100, nrow = 23), row.names = sort(append(as.character(1:22*5-5), "109")))
+
+for (j in 1:100){
+  for (i in 1:23){
+  Fixed[i,j] <- sum(pis[,i*4+(j)*92]==0)
+  }
+}  
+
+ConfIF <- data.frame(matrix(ncol = 3, nrow = 23), row.names = sort(append(as.character(1:22*5-5), "109")))
+colnames(ConfIF) <- c("Mean", "Lower", "Upper") 
+for (i in 1:23){
+  ConfIF[i,1] <- mean(as.numeric(Fixed[i,]))
+  ConfIF[i,2] <- quantile(as.numeric(Fixed[i,]), 0.025)
+  ConfIF[i,3] <- quantile(as.numeric(Fixed[i,]), 0.975)
+}
+
+######If use SD#####################################################
+#for (i in 1:23){
+#  ConfIF[i,1] <- mean(as.numeric(Fixed[i,]))
+#  ConfIF[i,2] <- ConfIF[i,1]-sd(as.numeric(Fixed[i,]))
+#  ConfIF[i,3] <- ConfIF[i,1]+sd(as.numeric(Fixed[i,]))
+#}
+####################################################################
+
+ConfIF[,"t"] <- as.numeric(rownames(ConfIF)) 
+ConfIF <- ConfIF[order(ConfIF$t),]
+write.csv(ConfIF, "q025_fixed.csv")
